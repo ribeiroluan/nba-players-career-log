16f21e3a15e42a5f941cb3f65cab9ead9e014fbb
Luan <savarizluan@gmail.com>
2023-03-30 10:27:00 -0300

    OOP refactor on push_to_redshift

--- push_to_redshift.py
+++ push_to_redshift.py
@@ -7,109 +7,119 @@ from dotenv import load_dotenv
 import sys
 import psycopg2
 from psycopg2.sql import Identifier, SQL
+import logging
+import datetime
 
-load_dotenv('.env')
-AWS_ID = getenv('AWS_ID')
-AWS_KEY = getenv('AWS_KEY')
-USERNAME = getenv('REDSHIFT_USERNAME')
-PASSWORD = getenv('REDSHIFT_PASSWORD')
-PORT = getenv('REDSHIFT_PORT')
-DATABASE = getenv('REDSHIFT_DATABASE')
-BUCKET_NAME = getenv('BUCKET_NAME')
-TABLE_NAME = "curry"
-ROLE = getenv('REDSHIFT_ROLE')
-HOST  = getenv('REDSHIFT_HOST')
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
+class UploadToRedshift:
 
-def connect_to_s3():
-    try:
-        conn = boto3.client('s3',
-                        aws_access_key_id = AWS_ID,
-                        aws_secret_access_key = AWS_KEY)
-        return conn
-    except Exception as e:
-        print(f"Can't connect to S3. Error: {e}")
-        sys.exit(1)
+    def __init__(self):
+        """Load AWS credentials"""
+        load_dotenv('.env')
+        self.aws_id = getenv('AWS_ID')
+        self.aws_key = getenv('AWS_KEY')
+        self.username = getenv('REDSHIFT_USERNAME')
+        self.password = getenv('REDSHIFT_PASSWORD')
+        self.port = getenv('REDSHIFT_PORT')
+        self.database = getenv('REDSHIFT_DATABASE')
+        self.bucket_name = getenv('BUCKET_NAME')
+        self.table_name = "curry"
+        self.role = getenv('REDSHIFT_ROLE')
+        self.host  = getenv('REDSHIFT_HOST')
 
-def get_latest_upload_date():
-    response = connect_to_s3().list_objects_v2(Bucket=BUCKET_NAME)
-    all = response['Contents'] 
-    latest = max(all, key=lambda x: x['LastModified'])
-    return latest['Key'].split('/')[1]
+    def connect_to_s3(self):
+        try:
+            conn = boto3.client('s3',
+                            aws_access_key_id = self.aws_id,
+                            aws_secret_access_key = self.aws_key)
+            return conn
+        except Exception as e:
+            print(f"Can't connect to S3. Error: {e}")
+            sys.exit(1)
 
-def get_filepath():
-    return f"s3://{BUCKET_NAME}/stephencurry/{get_latest_upload_date()}/regularseason.csv"
+    def get_latest_upload_date(self):
+        response = self.connect_to_s3().list_objects_v2(Bucket=self.bucket_name)
+        all = response['Contents'] 
+        latest = max(all, key=lambda x: x['LastModified'])
+        return latest['Key'].split('/')[1]
 
-def connect_to_redshift():
-    """Connect to Redshift instance"""
-    try:
-        rs_conn = psycopg2.connect(
-            dbname=DATABASE, user=USERNAME, password=PASSWORD, host = HOST, port=PORT
-        )
-        return rs_conn
-    except Exception as e:
-        print(f"Unable to connect to Redshift. Error {e}")
-        sys.exit(1)
+    def get_filepath(self):
+        return f"s3://{self.bucket_name}/stephencurry/{self.get_latest_upload_date()}/regularseason.csv"
 
-def load_data_into_redshift(rs_conn):
-    """Load data from S3 into Redshift"""
-    with rs_conn:
+    def connect_to_redshift(self):
+        """Connect to Redshift instance"""
+        try:
+            rs_conn = psycopg2.connect(
+                dbname=self.database, user=self.username, password=self.password, host = self.host, port=self.port
+            )
+            return rs_conn
+        except Exception as e:
+            print(f"Unable to connect to Redshift. Error {e}")
+            sys.exit(1)
 
-        cur = rs_conn.cursor()
-        cur.execute(sql_drop_old_table)
-        cur.execute(sql_create_new_table)
-        cur.execute(sql_copy_to_table)
+    def sql_drop_old_table(self):
+        return f"DROP TABLE IF EXISTS {self.table_name};"
+        
+    def sql_create_new_table(self):
+        return f""" CREATE TABLE {self.table_name} (
+                    GAME_ID int PRIMARY KEY,
+                    GAME_DATE timestamp,
+                    SEASON_TYPE varchar,
+                    SEASON_YEAR varchar,
+                    PLAYER_ID int,
+                    PLAYER_NAME varchar,
+                    NICKNAME varchar,
+                    TEAM_ID int,
+                    TEAM_ABBREVIATION varchar,
+                    TEAM_NAME varchar,
+                    MATCHUP varchar,
+                    WL varchar,
+                    MIN float,
+                    FGM int,
+                    FGA int,
+                    FG_PCT float,
+                    FG3M int,
+                    FG3A int,
+                    FG3_PCT float,
+                    FTM int,
+                    FTA int,
+                    FT_PCT float,
+                    OREB int,
+                    DREB int,
+                    REB int,
+                    AST int,
+                    TOV int,
+                    STL int,
+                    BLK int,
+                    BLKA int,
+                    PF int,
+                    PFD int,
+                    PTS int,
+                    PLUS_MINUS int,
+                    NBA_FANTASY_PTS float,
+                    DD2 bool,
+                    TD3 bool);"""
+    
+    def sql_copy_to_table(self):
+        return f""" COPY {self.table_name} (SEASON_YEAR,PLAYER_ID,PLAYER_NAME,NICKNAME,TEAM_ID,TEAM_ABBREVIATION,TEAM_NAME,GAME_ID,GAME_DATE,MATCHUP,WL,MIN,FGM,FGA,FG_PCT,FG3M,FG3A,FG3_PCT,FTM,FTA,FT_PCT,OREB,DREB,REB,AST,TOV,STL,BLK,BLKA,PF,PFD,PTS,PLUS_MINUS,NBA_FANTASY_PTS,DD2,TD3,SEASON_TYPE)
+                    FROM '{self.get_filepath()}'  iam_role '{self.role}' IGNOREHEADER 1 DELIMITER ',' CSV;""" 
 
-        rs_conn.commit()
-
-def main():
-    """Upload file from S3 to Redshift Table"""
-    rs_conn = connect_to_redshift()
-    load_data_into_redshift(rs_conn)
-
-sql_drop_old_table = f"DROP TABLE IF EXISTS {TABLE_NAME};"
-sql_create_new_table = f"""
-                    CREATE TABLE {TABLE_NAME} (
-                            GAME_ID int PRIMARY KEY,
-                            GAME_DATE timestamp,
-                            SEASON_TYPE varchar,
-                            SEASON_YEAR varchar,
-                            PLAYER_ID int,
-                            PLAYER_NAME varchar,
-                            NICKNAME varchar,
-                            TEAM_ID int,
-                            TEAM_ABBREVIATION varchar,
-                            TEAM_NAME varchar,
-                            MATCHUP varchar,
-                            WL varchar,
-                            MIN float,
-                            FGM int,
-                            FGA int,
-                            FG_PCT float,
-                            FG3M int,
-                            FG3A int,
-                            FG3_PCT float,
-                            FTM int,
-                            FTA int,
-                            FT_PCT float,
-                            OREB int,
-                            DREB int,
-                            REB int,
-                            AST int,
-                            TOV int,
-                            STL int,
-                            BLK int,
-                            BLKA int,
-                            PF int,
-                            PFD int,
-                            PTS int,
-                            PLUS_MINUS int,
-                            NBA_FANTASY_PTS float,
-                            DD2 bool,
-                            TD3 bool
-                        );"""
-sql_copy_to_table = f'''COPY {TABLE_NAME} (SEASON_YEAR,PLAYER_ID,PLAYER_NAME,NICKNAME,TEAM_ID,TEAM_ABBREVIATION,TEAM_NAME,GAME_ID,GAME_DATE,MATCHUP,WL,MIN,FGM,FGA,FG_PCT,FG3M,FG3A,FG3_PCT,FTM,FTA,FT_PCT,OREB,DREB,REB,AST,TOV,STL,BLK,BLKA,PF,PFD,PTS,PLUS_MINUS,NBA_FANTASY_PTS,DD2,TD3,SEASON_TYPE)
-                    FROM '{get_filepath()}'  iam_role '{ROLE}' IGNOREHEADER 1 DELIMITER ',' CSV;'''
-
-if __name__ == "__main__":
-    main()
+    def load_data_into_redshift(self, rs_conn):
+        """Load data from S3 into Redshift"""
+        with rs_conn:
+            try:
+                cur = rs_conn.cursor()
+                cur.execute(self.sql_drop_old_table())
+                cur.execute(self.sql_create_new_table())
+                cur.execute(self.sql_copy_to_table())
+                rs_conn.commit()
+                logger.info(f"S3 file {self.get_filepath()} uploaded to Redshift at {datetime.datetime.now()}")
+            except:
+                logger.info(f"Error copying S3 file {self.get_filepath()} to Redshift at {datetime.datetime.now()}")
+                
+    def copy(self):
+        """Upload file from S3 to Redshift Table"""
+        rs_conn = self.connect_to_redshift()
+        self.load_data_into_redshift(rs_conn)
