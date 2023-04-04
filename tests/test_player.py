c069d060412508ef46367d6215930728b3770ce7
Luan <savarizluan@gmail.com>
2023-04-04 09:56:58 -0300

    Added test_player_id

--- tests/test_player.py
+++ tests/test_player.py
@@ -25,6 +25,12 @@ class TestPlayerCareer:
         actual = player._check_if_season_type_exists()
         assert actual  == expected
 
+    @patch(target="nba_api.stats.static.players.find_players_by_full_name", return_value=[{"id":1234}])
+    def test_player_id(self, mock):
+        player = PlayerCareer(player_full_name="Valid player name", season_type="Valid season type")
+        actual = player.player_id
+        expected = 1234
+        assert actual == expected
 class TestDataCleaner:
     pass
 
