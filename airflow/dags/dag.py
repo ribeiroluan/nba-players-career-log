9c00ed2d992327e5525d08ba34ae3e82ed46f189
Luan <savarizluan@gmail.com>
2023-04-11 19:41:28 -0300

    Changes

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -2,26 +2,22 @@ import airflow
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
-#from code.player import PlayerCareer, DataCleaner, DataWriter, CleanFolder
-#from code.push_to_s3 import UploadToS3
-#from code.push_to_redshift import UploadToRedshift
-from datetime import datetime, timedelta
 
 schedule_interval = '@daily'
 start_date = days_ago(1)
-args = {
-    'owner': 'luan',
-    'start_date': airflow.utils.dates.days_ago(0),
-}
+args = {'owner': 'luan', 'start_date': start_date, 'retries': 1}
 
 with DAG(
-    dag_id= 'nba-player-career-log',
+    dag_id= 'nba-player-career-etl',
+    description= 'NBA career ETL',
     default_args= args,
-    schedule_interval=None,
-    dagrun_timeout=timedelta(minutes=60)
+    schedule_interval=schedule_interval,
+    catchup=True,
+    max_active_runs=1,
+    tags=['NBA ETL'],
 ) as dag:
 
-    extract_data = BashOperator(
-        task_id = "extract-data",
+    run_etl = BashOperator(
+        task_id = "nba-etl",
         bash_command = "python /opt/airflow/code/main.py" ,
         dag=dag)
